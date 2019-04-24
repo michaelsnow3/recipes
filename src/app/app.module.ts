6ed4afed7f8b4b3bd8fb6f7825de7ff7f6254ca1
michaelsnow3 <mikesnow444@gmail.com>
2019-04-24 14:07:35 -0400

    modified component directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,8 @@ import { HeaderComponent } from "./header/header.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListEditComponent } from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
-import { ShoppingListItemComponent } from "./shopping-list/shopping-list-item/shopping-list-item.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipes/recipe-item/recipe-item.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 
 @NgModule({
@@ -18,7 +17,6 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
     ShoppingListComponent,
     RecipesComponent,
     ShoppingListEditComponent,
-    ShoppingListItemComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent
