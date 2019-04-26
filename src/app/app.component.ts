321a79869e3edfcc6c264c3208c8666f04e05bf1
michaelsnow3 <mikesnow444@gmail.com>
2019-04-26 14:02:09 -0400

    changed var name in recipe list teplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipies';
-  page: string;
+  page = 'RECIPES';
 
   setPage = (pageName: string) => {
     this.page = pageName;
