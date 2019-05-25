b059a0362a0968ebe093f962f6d0e92d4fab8e0a
michaelsnow3 <39704990+michaelsnow3@users.noreply.github.com>
2019-05-25 13:10:28 -0400

    added recpipe ingredients to recipe detail template

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,12 +8,14 @@ export class RecipeService {
     new Recipe(
       'Testing recipe name 1',
       'testing description',
-      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'
+      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+      [new Ingredient('something', 1), new Ingredient('something else', 2)]
     ),
     new Recipe(
       'Testing recipe name 2',
       'testing description 2',
-      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'
+      'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+      [new Ingredient('asdf', 1), new Ingredient('fda', 2)]
     )
   ];
   getRecipes = () => {
