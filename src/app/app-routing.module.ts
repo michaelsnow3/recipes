18c589c8fe5cbf7da1429cee372475fb1b11f27d
michaelsnow3 <39704990+michaelsnow3@users.noreply.github.com>
2019-07-15 20:07:07 -0400

    added recipes and shopping list componenets to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes, { enableTracing: true })],
